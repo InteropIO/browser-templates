6e859276d79ce895417270a20aad2dc9f395aeae
Kalin Kostov <k.kostov.eu@gmail.com>
2023-10-30 11:38:06 +0200

    added io-cb-platform vanilla js template
    Signed-off-by: Kalin Kostov <k.kostov.eu@gmail.com>

---
+++ browser-platform-vanilla-js/public/index.js
@@ -0,0 +1,24 @@
+import IOBrowserPlatform from "./libs/browser.platform.es.js";
+
+import config from "./config.json" assert { type: "json" };
+
+document.getElementById("connection-status").innerHTML = "IO.Connect Browser Platform Not Connected";
+
+try {
+    const { io, platform } = await IOBrowserPlatform(config);
+
+    document.getElementById("connection-status").innerHTML = "IO.Connect Browser Platform Connected";
+
+    document.getElementById("connection-status").style.color = "green";
+
+    document.getElementById("api-version").innerHTML = `io.Connect Browser API ${io.version}`;
+
+    document.getElementById("platform-version").innerHTML = `io.Connect Browser Platform Version ${platform.version}`;
+
+    window.io = io;
+} catch (error) {
+    console.error(error);
+    document.getElementById("connection-status").innerHTML = error.message;
+
+    document.getElementById("connection-status").style.color = "red";
+}
