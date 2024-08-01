e9c119e311d00dd2bb591cbf74cf4dfd88989dd5
Kalina Georgieva <kalina.georgieva@interop.io>
2024-08-01 19:43:35 +0300

    Add new browser-platform template
    Signed-off-by: Kalina Georgieva <kalina.georgieva@interop.io>

---
+++ browser-platform-wsp-frame/README.md
@@ -0,0 +1,2 @@
+# Browser Platform in a Workspaces Frame
+This project provides a starter for React based io.Connect Browser application in a Workspaces Frame.
---
+++ browser-platform-wsp-frame/src/App.tsx
@@ -0,0 +1,17 @@
+import Workspaces from "@interopio/workspaces-ui-react";
+import { useIOConnect } from '@interopio/react-hooks';
+import "@interopio/workspaces-ui-react/dist/styles/workspaces.css"
+import './App.css'
+
+function App() {
+  useIOConnect(async (io) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (window as any).io = io;
+});
+  
+  return (
+    <Workspaces />
+  )
+}
+
+export default App
